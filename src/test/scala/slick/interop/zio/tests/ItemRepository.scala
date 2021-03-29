32bacb0ae0a2cd60c4dd91a21e34620634e5aa33
Remi Guittaut <remi.guittaut@gmail.com>
2021-03-29 22:44:43 +0200

    Issue-30 - Adding an extra ZIO.fromDBIO overload for DBIO effects requiring an execution context

--- src/test/scala/slick/interop/zio/tests/ItemRepository.scala
+++ src/test/scala/slick/interop/zio/tests/ItemRepository.scala
@@ -5,8 +5,8 @@ import zio.IO
 object ItemRepository {
 
   trait Service {
-
     def add(name: String): IO[Throwable, Long]
     def getById(id: Long): IO[Throwable, Option[Item]]
+    def upsert(name: String): IO[Throwable, Long]
   }
 }
