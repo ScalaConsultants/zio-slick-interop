7e97eae7b2f56bcc33e7fc3c0fffa1c071a68e73
jczuchnowski <jakub.czuchnowski@gmail.com>
2022-08-21 19:10:41 +0200

    Fix formatting

--- src/test/scala/slick/interop/zio/tests/SlickItemRepository.scala
+++ src/test/scala/slick/interop/zio/tests/SlickItemRepository.scala
@@ -11,41 +11,42 @@ object SlickItemRepository {
       for {
         db   <- ZIO.service[DatabaseProvider]
         repo <- db.profile.flatMap { profile =>
-          import profile.api._
+                  import profile.api._
 
-          val initialize = ZIO.fromDBIO(ItemsTable.table.schema.createIfNotExists)
+                  val initialize = ZIO.fromDBIO(ItemsTable.table.schema.createIfNotExists)
 
-          val dbLayer = ZLayer.succeed(db)
+                  val dbLayer = ZLayer.succeed(db)
 
-          val repository = new ItemRepository {
-            private val items = ItemsTable.table
+                  val repository = new ItemRepository {
+                    private val items = ItemsTable.table
 
-            def add(name: String): IO[Throwable, Long] =
-              ZIO
-                .fromDBIO((items returning items.map(_.id)) += Item(0L, name))
-                .provide(dbLayer)
+                    def add(name: String): IO[Throwable, Long] =
+                      ZIO
+                        .fromDBIO((items returning items.map(_.id)) += Item(0L, name))
+                        .provide(dbLayer)
 
-            def getById(id: Long): IO[Throwable, Option[Item]] = {
-              val query = items.filter(_.id === id).result
+                    def getById(id: Long): IO[Throwable, Option[Item]] = {
+                      val query = items.filter(_.id === id).result
 
-              ZIO.fromDBIO(query)
-                .map(_.headOption)
-                .provide(dbLayer)
-            }
+                      ZIO
+                        .fromDBIO(query)
+                        .map(_.headOption)
+                        .provide(dbLayer)
+                    }
 
-            def upsert(name: String): IO[Throwable, Long] =
-              ZIO.fromDBIO { implicit ec =>
-                (for {
-                  itemOpt <- items.filter(_.name === name).result.headOption
-                  id      <- itemOpt.fold[DBIOAction[Long, NoStream, Effect.Write]](
-                               (items returning items.map(_.id)) += Item(0L, name)
-                             )(item => (items.map(_.name) update name).map(_ => item.id))
-                } yield id).transactionally
-              }.provide(dbLayer)
-          }
+                    def upsert(name: String): IO[Throwable, Long] =
+                      ZIO.fromDBIO { implicit ec =>
+                        (for {
+                          itemOpt <- items.filter(_.name === name).result.headOption
+                          id      <- itemOpt.fold[DBIOAction[Long, NoStream, Effect.Write]](
+                                       (items returning items.map(_.id)) += Item(0L, name)
+                                     )(item => (items.map(_.name) update name).map(_ => item.id))
+                        } yield id).transactionally
+                      }.provide(dbLayer)
+                  }
 
-          initialize.as(repository).provide(dbLayer)
-        }
+                  initialize.as(repository).provide(dbLayer)
+                }
       } yield repo
     }
 }
